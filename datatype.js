f4e240a3c867292aab2dd36d1348fe872790a407
prajjal <prajjaldhar41@gmail.com>
2024-12-01 14:12:08 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,16 @@
+// let var const
+// var a = "hello";
+// console.log(a);
+
+// var a = "world";
+// console.log(a);
+
+// let a = "hello";
+// console.log(a);
+// a = "world";
+// console.log(a);
+
+// const a = "hello";
+// console.log(a);
+// a = "world";
+// console.log(a);
